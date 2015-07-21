ae3ef8e7b31751048618c8f2892bfa30f51a4ad3
srikarrohit <srikarrohit@gmail.com>
2015-07-21 10:48:09 +0530

    Biggestelementimentinanarray

---
+++ print/src/print.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class print {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+Scanner x = new Scanner(System.in);//ctrl+space gives full stmt and Ctrl+shift+o gives imports required
+	String y=x.nextLine();
+	System.out.println(y);
+	}
+
+}
